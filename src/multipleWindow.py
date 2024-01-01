b42973fd2b76b67a56d15166d357aaa16fad1065
Tanan2401 <zing9001@gmail.com>
2024-01-01 23:19:45 +0700

    Done function capture work monitor

--- src/multipleWindow.py
+++ src/multipleWindow.py
@@ -3,10 +3,16 @@ from tkinter import *
 # from Autocapture import *
 import keyboard
 import mss
-from win32api import GetMonitorInfo, MonitorFromPoint    
-    
+from win32api import GetMonitorInfo, MonitorFromPoint
+import pyautogui
+from screeninfo import get_monitors
+import pygetwindow as gw
+
+ 
 font_text = ("normal",14)
+font_text1 = ("normal",10)
 list_mode = ["Mode1", "Mode2"]
+
 monitor_info = GetMonitorInfo(MonitorFromPoint((0,0)))
 monitor_area = monitor_info.get("Monitor")
 work_area = monitor_info.get("Work")
@@ -17,45 +23,86 @@ class Screen1:
         self.data = None
         self.start_x = 0
         self.start_y = 0
-        self.stop_x = 1
-        self.stop_y = 1
-        
+        self.width = 1
+        self.height = 1
         self.count = 0
+        self.modeRegion  = None
         status1 = "         "
         status2 = "                   "
+        
         status1 = format(work_area[2]) + "x" + format(work_area[3])
         self.lbn_Mode1 = ttk.Label(root, text="Mode1:" + status1, borderwidth=1, relief="solid", font=font_text)
         self.lbn_Mode1.place(x = 0, y = 5)
         self.lbn_Mode2 = ttk.Label(root, text="Mode2:" + status2, borderwidth=1, relief="solid", font=font_text)
-        # height = self.lbn_Mode1.winfo_reqheight() # # 26 pixel
-        # print("Height:", height, "pixels")
         self.lbn_Mode2.place(x = 0, y = 35)
-        self.bt_goto_screen2 = Button(root, text="Go to Screen 2", font=font_text, command=self.gotoScreen2)
-        self.bt_goto_screen2.place(x = 0, y = 65)
+        height = self.lbn_Mode2.winfo_reqheight() # # 26 pixel
+        width = self.lbn_Mode2.winfo_reqwidth()
+        print("Height:", height, "pixels")
+        print("Width:", width, "pixels")
+        # self.bt_goto_screen2 = Button(root, text="Go to Screen 2", font=font_text, command=self.gotoScreen2)
+        # self.bt_goto_screen2.place(x = 0, y = 65)
+        self.bt_mode_region = Button(root,text = "SC1",font=font_text1,height=1,command=self.setRegionsc1)
+        self.bt_mode_region.place(x = 170, y = 34)     
+        # self.bt_mode_region = Button(root,text = "SC2",font=font_text1,height=1,command=self.setRegionsc2)
+        # self.bt_mode_region.place(x = 170 + 45, y = 34)     
         self.bt_show_data = Button(root,text = "Show data", font= font_text, command= self.showdata)
         self.bt_show_data.place(x = 0, y = 121)
-        # height = self.bt_show_data.winfo_reqheight() # # 31 pixel
-        # print("Height:", height, "pixels") 
-        keyboard.on_press(self.on_key_press)
+        height = self.bt_mode_region.winfo_reqheight() # # 31 pixel
+        width = self.bt_mode_region.winfo_reqwidth()
+        print("Height:", height, "pixels") 
+        print("Width:", width, "pixels")
+        keyboard.on_press(self.on_key_press)  
         
+    def setRegionsc1(self):
+        self.modeRegion = 0
+        self.gotoScreen2(self.modeRegion)
+        print("setRegionsc1")
+           
+    def setRegionsc2(self):
+        self.modeRegion = 1
+        self.gotoScreen2(self.modeRegion)
+        print("setRegionsc2")      
+            
     def on_key_press(self,event):
-        if event.name == "f2":
-            pos = [self.start_x,self.start_y,self.stop_x,self.stop_y]
-            self.captureRegion(pos)
+        pos = []
         if event.name == "f3":
-            pos = [0,0,work_area[2],work_area[3]]
-            self.captureRegion(pos)  
-        print("Key is pressed")
+            if self.modeRegion == 1:
+                self.start_x = self.monitor.x + self.start_x
+            pos = [self.start_x,self.start_y,self.width,self.height]
+            self.captureRegion(pos)
+            
+        if event.name == "f2" and keyboard.is_pressed("shift"):
+            monitors = get_monitors()
+            monitor = monitors[0] 
+            monitor_info = GetMonitorInfo(MonitorFromPoint((monitor.x,monitor.y)))
+            work_area = monitor_info.get("Work")
+            pos = [monitor.x,monitor.y,work_area[2],work_area[3]]
+            self.captureRegion(pos)
+            print("Shift F2 is pressed")
+            
+        elif event.name == "f2":
+            monitors = get_monitors()
+            monitor = monitors[1] 
+            monitor_info = GetMonitorInfo(MonitorFromPoint((monitor.x,monitor.y)))
+            work_area = monitor_info.get("Work")
+            print("work_area: ",work_area)
+            pos = [monitor.x,monitor.y,work_area[2]+abs(monitor.x),work_area[3]]
+            self.captureRegion(pos)
+            print("F2 is pressed")   
         
     def captureRegion(self,Pos):
         with mss.mss() as sct:
-            monitors = sct.monitors
-            monitor = sct.monitors[2]  # gia tri 1 OR 0        
+            # region = {
+            #     "left": min(Pos[0], Pos[2]),
+            #     "top": min(Pos[1], Pos[3]),
+            #     "width": abs(Pos[0] - Pos[2]),
+            #     "height": abs(Pos[1] - Pos[3])
+            # }
             region = {
-            "left": monitor["left"] + min(Pos[0], Pos[2]),
-            "top": monitor["top"] + min(Pos[1], Pos[3]),
-            "width": abs(Pos[0] - Pos[2]),
-            "height": abs(Pos[1] - Pos[3])
+                "left": Pos[0],
+                "top": Pos[1],
+                "width": Pos[2],
+                "height": Pos[3]
             }
             print(region)
             screenshot = sct.grab(region)
@@ -64,15 +111,15 @@ class Screen1:
             print("Capture Region" + format(self.count))
             self.count += 1
                    
-    def gotoScreen2(self):
-        self.root.withdraw()  
+    def gotoScreen2(self,screen_index):
+        self.data = [0,0,0,0]      
+        self.root.withdraw()              
         screen2 = Toplevel(self.root)
-        screen2.attributes('-fullscreen',True)
-        screen2.attributes('-alpha',0.1)
-        self.data = [0,0,0,0]
+        screen2.attributes('-alpha',0.5)
+        screen2.attributes("-fullscreen", True)
         sc = Screen2(screen2)
         sc.set_value(self.data)
-        
+
     def showdata(self):
         print("Show data")
         print(self.data)  
@@ -85,9 +132,9 @@ class Screen1:
     def update_screen(self):
         self.start_x = self.data[0]
         self.start_y = self.data[1]
-        self.stop_x = self.data[2]
-        self.stop_y = self.data[3] 
-        status2 = format(self.data[2] - self.data[0]) + "x" + format(self.data[3] - self.data[1])
+        self.width = self.data[2]
+        self.height= self.data[3] 
+        status2 = format(self.data[2]) + "x" + format(self.data[3])
         self.lbn_Mode2.config(text="Mode2:" + status2)
 
 
@@ -109,10 +156,10 @@ class Screen2:
         self.canvas.bind("<B1-Motion>", self.curent_capture)
         self.canvas.bind("<ButtonRelease-1>", self.stop_Capture)
         
-        bt_goto_screen1 = Button(root, text="Go to Screen 1", font=font_text, command=self.gotoScreen1)
-        bt_goto_screen1.grid(row=2, column=0, padx=5, pady=5)
-        bt_show_data = Button(root,text = "Show data", font= font_text, command= self.showdata)
-        bt_show_data.grid(row=3, column=0, padx=5, pady=5)
+        # bt_goto_screen1 = Button(root, text="Go to Screen 1", font=font_text, command=self.gotoScreen1)
+        # bt_goto_screen1.place(x = 0, y = 0)
+        # bt_show_data = Button(root,text = "Show data", font= font_text, command= self.showdata)
+        # bt_show_data.place(x = 0, y = 40)
         
     def start_capture(self, event):
         self.start_x = event.x
@@ -127,7 +174,7 @@ class Screen2:
         if self.current_rectangle:
             self.canvas.delete("rectangle")  
         x, y = self.start_x, self.start_y
-        print("drawing rectangle")
+        # print("drawing rectangle")
         self.current_rectangle = self.canvas.create_rectangle(x, y, self.current_x, self.current_y,outline="red",width=2, tags="rectangle")   
               
     def stop_Capture(self, event):
@@ -149,7 +196,10 @@ class Screen2:
     def gotoScreen1(self):
         self.root.destroy() 
         screen1 = Toplevel(self.root.master)   
-        self.data = [self.start_x,self.start_y,self.stop_x,self.stop_y]
+        screen1.geometry("300x200")
+        print("X: ",self.start_x," Y:",self.start_y)
+        print("Width: ",self.stop_x -self.start_x," Height: ",self.stop_y - self.start_y)
+        self.data = [self.start_x,self.start_y,self.stop_x -self.start_x,self.stop_y - self.start_y]
         sc = Screen1(screen1)
         sc.set_value(self.data)
         
@@ -163,8 +213,10 @@ class Screen2:
 def main():
     root = Tk()
     root.title("Main menu")
-    root.geometry("400x300")
-    app = Screen1(root)
+    window = gw.getWindowsWithTitle("Main menu")
+    print(window)
+    root.geometry("300x200")
+    app = Screen1(root) 
     root.mainloop()
 
 if __name__ == "__main__":
